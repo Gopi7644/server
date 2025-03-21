7cd2d2a6a9e43b6c265e6822823af7ce31aec7eb
Gopi Kumar <gopikumar764482@gmail.com>
2025-03-21 17:45:55 +0530

    id field added

--- controller/userController.js
+++ controller/userController.js
@@ -1,19 +1,12 @@
 import { User } from "../model/user.model.js";
+import { v4 as uuidv4 } from "uuid"; // Import uuid library
 
 export const create = async (req, res) => {
   try {
-
-
-
-    
-    const userData = new User(req.body);
+    const userData = new User({ ...req.body, id: uuidv4() }); // Assign a unique ID
     if (!userData) {
       res.status(404).json({ msg: "User data Not Found" });
     }
-
-
-
-
     const savedData = await userData.save();
     res.status(200).json(savedData);
   } catch (err) {
@@ -34,45 +27,44 @@ export const getAll = async (req, res) => {
 };
 
 export const getById = async (req, res) => {
-    try{
-        const id = req.params.id;
-        const userData = await User.findById(id);
-        if(!userData){
-            res.status(404).json({ msg: "User data Not Found" });
-        }
-        res.status(200).json(userData);
+  try {
+    const id = req.params.id;
+    const userData = await User.findById(id);
+    if (!userData) {
+      res.status(404).json({ msg: "User data Not Found" });
     }
-    catch(err){
-        res.status(500).json({ message: err.message });
-    }
-}
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 export const update = async (req, res) => {
-    try{
-        const id = req.params.id;
-        const userData = await User.findById(id)
-        if(!userData){
-            res.status(404).json({ msg: "User data Not Found" });
-        }
-        const updatedData = await User.findByIdAndUpdate(id, req.body, {new: true});
-        res.status(200).json(updatedData);
+  try {
+    const id = req.params.id;
+    const userData = await User.findById(id);
+    if (!userData) {
+      res.status(404).json({ msg: "User data Not Found" });
     }
-    catch(err){
-        res.status(500).json({ message: err.message });
-    }
-}
+    const updatedData = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedData);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
 
 export const deleteuser = async (req, res) => {
-    try{
-        const id = req.params.id;
-        const userData = await User.findById(id);
-        if(!userData){
-            res.status(404).json({ msg: "User data Not Found" });
-        }
-        await User.findByIdAndDelete(id);
-        res.status(200).json({msg: "User deleted successfully"});
+  try {
+    const id = req.params.id;
+    const userData = await User.findById(id);
+    if (!userData) {
+      res.status(404).json({ msg: "User data Not Found" });
     }
-    catch(err){
-        res.status(500).json({ message: err.message });
-    }
-}
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ msg: "User deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- model/user.model.js
+++ model/user.model.js
@@ -1,24 +1,28 @@
 import mongoose from "mongoose";
 
 const usermodel = new mongoose.Schema({
-    fname:{
-        type: String,
-        required: true
-    },
-    lname:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    message:{
-        type: String,
-        required: true
-    },
-})
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+});
 
-
-export const User = mongoose.model("User", usermodel)
+export const User = mongoose.model("User", usermodel);
